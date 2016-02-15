c6356ff09e9021d92cbc7020d492095749f03a06
Ryan Krolick <rkrolick@gmail.com>
2016-02-15 12:53:24 -0500

    Added code for end game handling

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var nodes=[["You are in the starting room.  What room would you like to goto next?","Room 1",1,"Room 2",2,"Room 3",3],
-           ["You are in room 1.  What room would you like to goto next?","Room 5",5,"Room 6",6,"Room 7",7],
-           ["You are in room 2.  What room would you like to goto next?","Room 8",8,"Room 9",9,"Room 10",10],
-           ["You are in room 3.  What room would you like to goto next?","Room 11",11,"Room 12",12,"Room 4",4],
-           ["You are in room 4.  What room would you like to goto next?","Room 13",13,"Room 14",14,"Room 15",15],
+var nodes=[["You are in the starting room.  What room would you like to goto next?","room 1",1,"room 2",2,"room 3",3],
+           ["You are in room 1.  What room would you like to goto next?","room 5",5,"room 6",6,"room 7",7],
+           ["You are in room 2.  What room would you like to goto next?","room 8",8,"room 9",9,"room 10",10],
+           ["You are in room 3.  What room would you like to goto next?","room 11",11,"room 12",12,"room 4",4],
+           ["You are in room 4.  What room would you like to goto next?","room 13",13,"room 14",14,"room 15",15],
            ["You have arrived at destination 1!","END",0,"END",0,"END",0],
            ["You have arrived at destination 2!","END",0,"END",0,"END",0],
            ["You have arrived at destination 3!","END",0,"END",0,"END",0],
@@ -14,7 +14,7 @@ var nodes=[["You are in the starting room.  What room would you like to goto nex
            ["You have arrived at destination 9!","END",0,"END",0,"END",0],
            ["You have arrived at destination 10!","END",0,"END",0,"END",0],
            ["You have arrived at destination 11!","END",0,"END",0,"END",0]]
-var curNode;
+var curNode, endLink, eog;
 
 function ProcIn(){
   var inputLine = document.getElementById("in");
@@ -32,13 +32,15 @@ function WriteOut(outTxt){
 }
 
 function StartUp(){
+    eog = false;
+    endLink = "https://www.youtube.com/watch?v=iJ4T9CQA0UM";
     WriteOut("Type HELP for a list of commands.");
     GotoNode(0);
 }
 
 function ParseIn(txt){
   txt = txt.replace(/\r?\n|\r/g,"")
-  //txt = txt.toLowerCase();
+  txt = txt.toLowerCase();
   //var test = txt.split(" ");
   switch(txt) {
     case "help": DispHelp(); break;
@@ -50,15 +52,31 @@ function ParseIn(txt){
     case "1":
     case nodes[curNode][1].charAt(0):
     case nodes[curNode][1]:
-      GotoNode(nodes[curNode][2]); break;
+      if(!eog){GotoNode(nodes[curNode][2]);}
+      else {StartUp();}
+      break;
     case "2":
     case nodes[curNode][3].charAt(0):
     case nodes[curNode][3]:
-      GotoNode(nodes[curNode][4]); break;
+      if(!eog){GotoNode(nodes[curNode][4]);}
+      else {EndGame();}
+      break;
     case "3":
     case nodes[curNode][5].charAt(0):
     case nodes[curNode][5]:
-      GotoNode(nodes[curNode][6]); break;
+      if(!eog){GotoNode(nodes[curNode][6]);}
+      else {WriteOut("Command not recognized.");}
+      break;
+    case "yes":
+    case "y":
+      if(eog){StartUp();}
+      else {WriteOut("Command not recognized.");}
+      break;
+    case "no":
+    case "n":
+      if (eog){EndGame();}
+      else {WriteOut("Command not recognized.");}
+      break;
     default:
       WriteOut("Command not recognized.");
   }
@@ -83,12 +101,23 @@ function DispHelp(){
 
 function GotoNode(nodeNumber){
   curNode = nodeNumber;
+  if (curNode > 4){eog = true;}
   WriteOut(nodes[curNode][0]);
-  WriteOut("1) " + nodes[curNode][1]);
-  WriteOut("2) " + nodes[curNode][3]);
-  WriteOut("3) " + nodes[curNode][5]);
+
+  if(!eog){
+    WriteOut("1) " + nodes[curNode][1]);
+    WriteOut("2) " + nodes[curNode][3]);
+    WriteOut("3) " + nodes[curNode][5]);
+  }
+  else {
+    endLink=nodes[curNode][4];
+    WriteOut("You've reached the end of your adventure.");
+    WriteOut("Would you like to play again?");
+    WriteOut("1) yes");
+    WriteOut("2) no");
+  }
 }
 
 function EndGame(){
-  window.location.assign("https://www.youtube.com/watch?v=iJ4T9CQA0UM");
+  window.location.assign(endLink);
 }
